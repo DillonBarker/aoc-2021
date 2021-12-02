2addf018ee77412e8c8ebadbc071d1f1da5d0898
dillon.barker <dillon.barker@testevolve.com>
2021-12-02 09:15:07 +0000

    day 3 first pass

---
+++ day2/part1/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"aoc/commons/read"
+)
+
+func getLineAsInt(line string) (int, error) {
+	lineAsInt, err := strconv.Atoi(line)
+	if err != nil {
+		os.Exit(2)
+	}
+	return lineAsInt, err
+}
+
+func main() {
+	lines, err := read.ReadFile("day2")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	horCtr := 0
+	verCtr := 0
+
+	for i, line := range lines {
+		slice := strings.Split(line, " ")
+		fmt.Println(i, slice[0], slice[1])
+
+		sliceAsInt, err := getLineAsInt(slice[1])
+
+		if slice[0] == "forward" {
+			horCtr = horCtr + sliceAsInt
+		}
+
+		if slice[0] == "down" {
+			verCtr += sliceAsInt
+		}
+
+		if slice[0] == "up" {
+			verCtr += sliceAsInt
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	fmt.Println(horCtr * verCtr)
+}
