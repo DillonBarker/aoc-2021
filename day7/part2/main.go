907ae24de31b5fa9839cee4c75b40f69f1ccf7ca
dillon.barker <dillon.barker@testevolve.com>
2021-12-09 19:22:49 +0000

    part 2 day 7 nice solution

--- day7/part2/main.go
+++ day7/part2/main.go
@@ -5,38 +5,34 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 	"strings"
 )
 
 func main() {
 	crabs := getInput()
-	optimalPos := medianCrab(crabs)
+	optimalPos := meanCrab(crabs)
 	fuel := 0
 	for _, crab := range crabs {
 		if crab != optimalPos {
 			diff := diff(crab, optimalPos)
-			fuel += diff
+			for i := 0; i < diff; i++ {
+				fuel += i + 1
+			}
 		}
 	}
 	fmt.Println(fuel)
 }
 
-func medianCrab(crabs []int) int {
-	sortedCrabs := sortCrabs(crabs)
-
-	middleCrab := len(sortedCrabs) / 2
-
-	if middleCrab%2 != 0 {
-		return sortedCrabs[middleCrab]
-	} else {
-		return (sortedCrabs[middleCrab-1] + sortedCrabs[middleCrab]) / 2
+func meanCrab(crabs []int) int {
+	n := len(crabs)
+	sum := 0
+	for i := 0; i < n; i++ {
+		sum += (crabs[i])
 	}
-}
-
-func sortCrabs(crabs []int) []int { // assuming no tie
-	sort.Ints(crabs)
-	return crabs
+	avg := (float64(sum)) / (float64(n))
+	avgInt := int(avg)
+	fmt.Println(avg)
+	return avgInt
 }
 
 func diff(a, b int) int {
